59869f21384776a7afb40d28d579f2571970aa1e
Rodrigo Jorge <rodrigofjorge@gmail.com>
2026-04-16 11:01:46 -0300

    first commit

---
+++ src/test/java/org/projeto/snowflakekafkapipeline/SnowflakeKafkaPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projeto.snowflakekafkapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowflakeKafkaPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
